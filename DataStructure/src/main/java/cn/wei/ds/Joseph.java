ef3e298938e13fe2fabfd64d4cdce73793bba4e7
SunKuso <372044040@qq.com>
2020-07-06 23:08:40 +0800

    修改:[数据结构]修改约瑟夫问题解法中的文件结构

--- DataStructure/src/main/java/cn/wei/ds/Joseph.java
+++ DataStructure/src/main/java/cn/wei/ds/Joseph.java
@@ -66,39 +66,3 @@ public class Joseph {
     }
 }
 
-class Node {
-    private int index;
-    private Node next;
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public Node(int index, Node next) {
-            this.index = index;
-            this.next = next;
-    }
-
-    public Node() {
-    }
-
-    @Override
-    public String toString() {
-        return "Node{" +
-                "index=" + index +
-                "next=" + next.index +
-                '}';
-    }
-}
