fb2d70a83b6525c3f61efcd453fd220165a5a4b5
SunKuso <372044040@qq.com>
2020-07-14 00:26:12 +0800

    修改:[数据结构]约瑟夫问题解法中的注释

--- DataStructure/src/main/java/cn/wei/ds/Joseph.java
+++ DataStructure/src/main/java/cn/wei/ds/Joseph.java
@@ -1,10 +1,21 @@
 package cn.wei.ds;
 
 public class Joseph {
+    //头节点
     private Node first;
+    //辅助节点,位于头节点之后
     private Node helper;
     private int length;
 
+    /*
+     * <p>构建一个单向循环链表</p>
+     * @param length 链表长度
+     * @version 0.1.0
+     * @return cn.wei.ds.Node 头节点
+     * @author SunWei
+     * @date 2020/7/14 0:11
+     * @since 0.1.0
+    */
     public Node generateLinkedList(int length) {
         if (length < 1) {
             return null;
@@ -12,10 +23,10 @@ public class Joseph {
         this.length = length;
         Node current = null;
         for (int i = 0; i < length; i++) {
-            if (i == 0) {
+            if (i == 0) { //第一个节点自己指向自己
                 first = new Node(0, first);
                 current = first;
-            } else {
+            } else {//插入节点
                 Node node = new Node(i, first);
                 current.setNext(node);
                 current = node;
@@ -24,6 +35,15 @@ public class Joseph {
         return first;
     }
 
+    /*
+     * <p>打印链表情况</p>
+     * @param
+     * @version 0.1.0
+     * @return void
+     * @author SunWei
+     * @date 2020/7/14 0:15
+     * @since 0.1.0
+    */
     public void printList() {
         Node next = first;
         while (next != first) {
@@ -32,10 +52,21 @@ public class Joseph {
         }
     }
 
+    /*
+     * <p>约瑟夫问题实现</p>
+     * @param startIndex 起始位置
+     * @param interval 计数间隔
+     * @version 0.1.0
+     * @return void
+     * @author SunWei
+     * @date 2020/7/14 0:15
+     * @since 0.1.0
+    */
     public void resolveJoseph(int startIndex, int interval) {
         if (startIndex > length) {
             return;
         }
+        //找到起始位置,将头节点指向起始位置,辅助节点指向头节点之前
         for (int i = 0; i < startIndex; i++) {
             if (first.getIndex() == startIndex) {
                 break;
@@ -43,15 +74,16 @@ public class Joseph {
             helper = first;
             first = first.getNext();
         }
+
         while (true) {
-            if (helper == first) {
+            if (helper == first) {//头结点和辅助节点都指向同一位置,表示链表中只有一个元素了
                 break;
             }
-            for (int i = 0; i < interval - 1; i++) {
+            for (int i = 0; i < interval - 1; i++) {//遍历链表,移动头节点和辅助节点
                 helper = helper.getNext();
                 first = first.getNext();
             }
-            System.out.println(first.getIndex());
+            System.out.println(first.getIndex());//当前节点需要剔除
             first = first.getNext();
             helper.setNext(first);
         }
